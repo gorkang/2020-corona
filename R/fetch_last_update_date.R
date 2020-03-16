293817afee5e6e8475122bb12cf9237b5db4ac14
gorkang <gorkang@gmail.com>
2020-03-16 19:28:55 -0300

    use download_or_load()

--- R/fetch_last_update_date.R
+++ R/fetch_last_update_date.R
@@ -3,16 +3,9 @@ fetch_last_update_date <-
     
     function() {
 
-      # require(httr)
-      maxTimes <- 10
+      download_or_load("outputs/last_updte_time.html", URL = "https://api.github.com/repos/CSSEGISandData/COVID-19/commits?path=csse_covid_19_data%2Fcsse_covid_19_time_series%2Ftime_series_19-covid-Confirmed.csv&page=1&per_page=1")
       
-      url = 'https://api.github.com/repos/CSSEGISandData/COVID-19/commits?path=csse_covid_19_data%2Fcsse_covid_19_time_series%2Ftime_series_19-covid-Confirmed.csv&page=1&per_page=1'
-      RETRY(verb = "GET", url = url, times = maxTimes, quiet = FALSE, terminate_on = NULL, encode = c("multipart", "form", "json", "raw")) %>% 
-        write_lines(path = "temp.html")
-      
-      # download_or_load("temp_worldometers.html", URL = "https://www.worldometers.info/coronavirus/#countries")
-      
-      last_commit = jsonlite::read_json(here::here("temp.html"))
+      last_commit = jsonlite::read_json(here::here("outputs/last_updte_time.html"))
       date_last_commit_raw = last_commit[[1]]$commit$author$date
       strptime(date_last_commit_raw, "%FT%T", tz = "GMT")
       }
